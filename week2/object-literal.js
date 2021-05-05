1b19d49e6831660ecb6fabbcb8182cdd7dddbd6b
Tyler Williams <tyler@coolsoftware.dev>
2021-05-04 19:34:01 -0600

    complete object literal

--- week2/object-literal.js
+++ week2/object-literal.js
@@ -6,3 +6,54 @@
  * #5 Convert the object literal to an Class
  * #6 Then call its method and output to the console.
  */
+
+// Object literal with properties.
+// Two methods: findBanana filters an array for strings matching `banana`.
+// eatBanana pops an item from an array of `banana`s (or any array, technically and logs it out.
+const monkey = {
+    hands: 2,
+    feet: 2,
+    tail: true,
+    findBanana: function (fruits) {
+        return fruits.filter(fruit => fruit === 'banana');
+    },
+    eatBanana: function (bananas) {
+        const banana = bananas.pop();
+        console.log(`Eating ${banana}`);
+    }
+}
+
+// Create an array of random fruits
+const randomFruits = ['apple', 'banana', 'orange'];
+// Find the banana in the random fruits
+const banana = monkey.findBanana(randomFruits);
+// Log out the banana array
+console.log(banana);
+
+// Monkey class which accepts hands, feet, tail arguments to its constructor
+// It has methods for `findBanana` and `eatBanana` as well, whcih do the same.
+class Monkey {
+    constructor(hands, feet, tail) {
+        this.hands = hands;
+        this.feet = feet;
+        this.tail = tail;
+    }
+    findBanana(fruits) {
+        return fruits.filter(fruit => fruit === 'banana');
+    }
+    eatBanana(bananas) {
+        const banana = bananas.pop();
+        console.log(`Eating ${banana}`);
+    }
+}
+
+// Instantiate George, a Monkey.
+const george = new Monkey(2, 2, true);
+// Ask George to find a banana
+const bananas = george.findBanana(randomFruits);
+// Log out the bananas he found.
+console.log(bananas);
+// Let George eat a banana
+george.eatBanana(bananas);
+// Check the bananas - they should be empty.
+console.log(bananas);
