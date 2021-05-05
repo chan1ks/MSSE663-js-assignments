bd8f0c480dd0f00adf164003bbad6c24fa6e87ea
Tyler Williams <tyler@coolsoftware.dev>
2021-05-04 18:36:12 -0600

    complete arrays and loops exercises

--- week2/arrays-and-loops.js
+++ week2/arrays-and-loops.js
@@ -1,6 +1,6 @@
 // #1 Take an array of numbers and make them strings
 function stringItUp(arr) {
-	// your code here
+	return arr.map(item => item.toString())
 }
 
 console.log(stringItUp([2, 5, 100]));
@@ -8,7 +8,7 @@ console.log(stringItUp([2, 5, 100]));
 
 // #2 Make an array of strings of the names
 function namesOnly(arr) {
-	// your code here
+	return arr.map(item => item.name)
 }
 
 console.log(
@@ -39,7 +39,8 @@ console.log(
 
 // #3 Filter the output array from #2.
 function oldOnesOnly(arr) {
-	// your code here
+	return arr.map(item => item.age)
+	.filter(age => age >= 80)
 }
 console.log(
 	oldOnesOnly([
