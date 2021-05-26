ab5bfc68816dc25c184a50426679e841eb6daada
Benjamin Bruffey <bruffster@gmail.com>
2021-05-25 22:01:46 -0600

    week4 homework

--- week4/src/spread.ts
+++ week4/src/spread.ts
@@ -5,13 +5,16 @@ export const originalNumbers = numbers.map(number => number);
 console.log(originalNumbers);
 
 // refactor here
+export const numbers2 = [...numbers];
+console.log(numbers2.map(num => num));
 
 // #2 Combining arrays
 export const winners = ['first'];
 export const runnerUps = ['second', 'third', 'fourth', 'fifth'];
 
 // refactor here
-
+export const finishers = [...winners, ...runnerUps];
+console.log(finishers);
 // #3 Combining objects
 export const bob = {
   man: 'Bob'
@@ -24,6 +27,14 @@ export const kyle = {
 export const originalPersons = Object.assign(bob, kyle);
 
 // refactor here
+export const people = {
+  ...bob,
+  ...kyle
+}
+
+console.log(originalPersons);
+console.log(people);
+
 
 // #4 Modifying values in arrays of objects
 export const data = [
@@ -54,3 +65,19 @@ export const originalUpdates = data.map(task => {
 });
 
 // refactor here
+const newUpdates = data.map((task): object => {
+  if(task.id === update.id) {
+    return {...task, ...update};
+  }
+  return task;
+});
+
+console.log('data');
+console.log(data);
+console.log('Morgan\'s stuff');
+console.log(originalUpdates)
+console.log('my stuff');
+console.log(newUpdates);
+console.log('data');
+console.log(data);
+
